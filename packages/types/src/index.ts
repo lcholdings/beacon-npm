0001a64275fbc5ce1bd3f11d0531e54a326eac02
Lazyllama <me@lazyllama.xyz>
2025-06-11 11:18:11 +0200

    feat: update version to 0.1.0-alpha.23 and remove unused Zod schemas for player identifiers, server socket, and staff events

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -2,8 +2,3 @@
 export * from "./types/serverSocket";
 export * from "./types/staffEvents";
 export * from "./types/playerIdentifiers";
-
-// Zod Schemas
-export * from "./schemas/serverSocket";
-export * from "./schemas/playerIdentifiers";
-export * from "./schemas/staffEvents";
